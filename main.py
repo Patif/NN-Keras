13e930142604f0594e573d6b2441fcecd717605d
Patrycja Frąckiewicz <65953083+Patif@users.noreply.github.com>
2021-12-03 14:33:21 +0100

    Tests v2

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ import tensorflow as tf
 if __name__ == "__main__":
     epochs = 8
     (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
-    print("Fully connected")
     fully_connected = tf.keras.models.Sequential(
         [tf.keras.layers.Flatten(input_shape=(28, 28)),
          tf.keras.layers.Dense(180, activation='relu'),
          tf.keras.layers.Dense(128, activation='relu'),
          tf.keras.layers.Dense(10)])
+    print("Fully connected")
     fully_connected.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                             metrics=['accuracy'])
     fully_connected.fit(X_train, Y_train, epochs=epochs, verbose=2)
     fully_connected.evaluate(X_test, Y_test, verbose=2)
-
+    #
     X_train = X_train.reshape(60000, 28, 28, 1)
     X_test = X_test.reshape(10000, 28, 28, 1)
     Y_train = to_categorical(Y_train)
     Y_test = to_categorical(Y_test)
+    print()
     print("Convolutional")
     convolutional = tf.keras.models.Sequential(
         [tf.keras.layers.Conv2D(16, kernel_size=3, activation="relu", input_shape=(28, 28, 1)),
@@ -31,7 +32,8 @@ if __name__ == "__main__":
     convolutional.fit(X_train, Y_train, epochs=epochs, verbose=2)
     convolutional.evaluate(X_test, Y_test, verbose=2)
 
-    print("Convolutional + pooling")
+    print()
+    print("Convolutional + max pooling")
     convolutional = tf.keras.models.Sequential(
         [tf.keras.layers.Conv2D(16, kernel_size=3, activation="relu", input_shape=(28, 28, 1)),
          tf.keras.layers.MaxPooling2D(),
@@ -41,3 +43,16 @@ if __name__ == "__main__":
                           metrics=['accuracy'])
     convolutional.fit(X_train, Y_train, epochs=epochs, verbose=2)
     convolutional.evaluate(X_test, Y_test, verbose=2)
+
+    for i in range(10):
+        print()
+        print(i, " Convolutional + avg pooling")
+        convolutional = tf.keras.models.Sequential(
+            [tf.keras.layers.Conv2D(16, kernel_size=6, activation="relu", input_shape=(28, 28, 1)),
+             tf.keras.layers.AveragePooling2D(),
+             tf.keras.layers.Flatten(),
+             tf.keras.layers.Dense(10, activation="softmax")])
+        convolutional.compile(optimizer='adam', loss='categorical_crossentropy',
+                              metrics=['accuracy'])
+        convolutional.fit(X_train, Y_train, epochs=epochs, verbose=2)
+        convolutional.evaluate(X_test, Y_test, verbose=2)
